d7b36575deaca3fb40af5b27830d10c9c2ab462b
liuxiangrong <liuxiangrong@youmi.net>
2016-12-21 17:17:37 +0800

    default

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const async = require('async');
 const url = require('url');
 const News = require("./model/models");
-const getRandomUseragent = require('./util/utils');
+const utils = require('./util/utils');
 
 const crawler_url = 'http://baijia.baidu.com';
 // listUrlExample: http://baijia.baidu.com/?tn=listarticle&labelid=100
@@ -19,29 +19,50 @@ let pageUrlArr = []; // 文章url数组
 
 // config crawler
 crawler.discoverResources = false; // 禁用自动搜索资源
-crawler.userAgent = getRandomUseragent();
+crawler.userAgent = utils.getRandomUseragent();
+crawler.respectRobotsTxt = false;
 
 crawler.on("fetchcomplete", (queueItem, data, res) => {
     const $ = cheerio.load(data);
+    // console.log(queueItem.url.split("/").indexOf("article") > -1);
+    console.log(queueItem.url);
+    referrer = {
+        url: queueItem.url,
+        depth: queueItem.depth + 1
+    };
 
     // fetch list url
-    if (queueItem.url === 'http://baijia.baidu.com') {
+    if (queueItem.url === 'http://baijia.baidu.com/') {
         $("#menu a").each(function(index, value){
-            listUrlArr.push({$(this).attr("href")});
+            if ($(this).attr("href") !== "http://baijia.baidu.com") {
+                listUrlArr.push($(this).attr("href"));
+            }
         });
         // push to crawler queue
-        pushToCrawler(listUrlArr, queueItem.referrer);
+        pushToCrawler(listUrlArr, referrer);
+        return false;
     }
 
     // fetch page url
-    if (url.parse(queueItem.url).query.tn === "listarticle") {
+    if (url.parse(queueItem.url, true).query.tn === "listarticle") {
         let lastId = $(".feeds-item:last-child").attr("id").replace(/[item-]/g,"");
+        let tempArr = [];
 
         $("#feeds .feeds-item").each(function(index, value){
-            pageUrlArr.push($(this).find("h3 a").attr("href"));
+            // pageUrlArr.push($(this).find("h3 a").attr("href"));
+            tempArr.push($(this).find("h3 a").attr("href"));
         });
 
-        goAjax(2, url.parse(queueItem.url, true).query.labelid, lastId);
+        /*setTimeout(function() {
+            console.log("start ajax");
+            // goAjax(2, url.parse(queueItem.url, true).query.labelid, lastId, queueItem);
+            goAjax(2, url.parse(queueItem.url, true).query.labelid);
+        }, 0);*/
+
+        // pushToCrawler(pageUrlArr.splice(0, pageUrlArr.length), queueItem.referrer);
+        pushToCrawler(tempArr, referrer);
+
+        return false;
     }
 
     // fetch page info
@@ -111,39 +132,71 @@ crawler.on("fetchcomplete", (queueItem, data, res) => {
     }
 
     // push to crawler queue
-    if (pageUrlArr.length > 0) {
+    /*if (pageUrlArr.length > 0) {
         pushToCrawler(pageUrlArr.splice(0, 10), queueItem.referrer);
-    }
+    }*/
 
 });
 
 // 发送ajax请求文章列表分页
+// function goAjax (page=2, labelid, prevarticalid, queueItem) {
 function goAjax (page=2, labelid, prevarticalid) {
-    let url = `http://baijia.baidu.com/ajax/labellatestarticle?page=${page}&pagesize=100&labelid=${labelid}&prevarticalid=${prevarticalid}`
+    let url = `http://baijia.baidu.com/ajax/labellatestarticle?page=${page}&pagesize=20&labelid=${labelid}&prevarticalid=${prevarticalid}`
     request(url, function(error, response, body){
         let list = JSON.parse(body).data.list;
-        let lastId = list[list.length-1].ID;
+        let lastId;
 
         // 出错的时候，list为undefined
         if (!list) {
+            console.log("list fucked");
             return false;
         }
+
+        lastId = list[list.length-1].ID;
 
         for (let i = 0; i < list.length; i++) {
             pageUrlArr.push(list[i].m_display_url);
         }
 
         console.log(pageUrlArr.length);
+        /*if (pageUrlArr.length > 0) {
+            pushToCrawler(pageUrlArr.splice(0, 10), queueItem.referrer);
+        }*/
+        // goAjax(page+1, labelid, lastId, queueItem);
         goAjax(page+1, labelid, lastId);
     })
 }
 
 // 推送到爬虫队列
 function pushToCrawler (urlArr, referrerQueueItem) {
+    console.log("here i push");
     for (let i=0; i<urlArr.length; i++) {
         crawler.queueURL(urlArr[i], referrerQueueItem, false);
     }
 }
+
+crawler.on('queueerror', (error, queueItem) => {
+    console.log(`排队${queueItem.url}出错`);
+});
+crawler.on('fetchdataerror', (error, queueItem) => {
+    console.log(`爬取${queueItem.url}出错`);
+})
+crawler.on('downloadprevented', (queueItem, res) => {
+    console.log(`下载${queueItem.url}出错`);
+});
+crawler.on('fetchdisallowed', (queueItem) => {
+    console.log(`不准爬取${queueItem.url}`);
+});
+crawler.on('queueduplicate', (queueItem)=> {
+    console.log(`队列重复${queueItem.url}`);
+});
+crawler.on('fetchtimeout', (queueItem) => {
+    console.log(`爬取超时${queueItem.url}`);
+});
+crawler.on('fetchclienterror', (queueItem, errorData) => {
+    console.log(`客户端错误${queueItem.url}`);
+});
+
 
 crawler.start();
 
